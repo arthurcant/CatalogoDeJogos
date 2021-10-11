9827eb336f29c4065e9ce1a71383d93feba9e839
arthurcant <arthurbig19@gmail.com>
2021-10-11 20:28:44 -0300

    #04 Atualizando o projeto.

--- Services/JogoService.cs
+++ Services/JogoService.cs
@@ -1,7 +1,13 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CatalogoDeJogos.Repositories;
+using CatalogoDeJogos.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
 namespace CatalogoDeJogos.Services
 {
-    public class JogoService
+    public class JogoService : IJogoService
     {
-        
+            
     }
 }
